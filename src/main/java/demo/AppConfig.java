5fa08f7d85760e5b6a4f037d897367493cb02d3d
Matt Reynolds <matthew.reynolds@staples.com>
2015-03-09 12:50:24 -0600

    adding codahale JVM metrics

--- src/main/java/demo/AppConfig.java
+++ src/main/java/demo/AppConfig.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.lang.management.ManagementFactory;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.LoggerFactory;
@@ -7,9 +9,11 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.codahale.metrics.ConsoleReporter;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Slf4jReporter;
+import com.codahale.metrics.*;
+import com.codahale.metrics.jvm.BufferPoolMetricSet;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 
 @Component
 public class AppConfig implements InitializingBean {
@@ -17,18 +21,49 @@ public class AppConfig implements InitializingBean {
 	@Autowired
 	MetricRegistry metricRegistry;
 
+	//need separate registry for threads data as it does not mix well with regular spring metrics
+	//probably down to name=threads.deadlocks, value=[]
+	final MetricRegistry threadRegistry = new MetricRegistry();
+	
+	//jvm metrics setup from http://jansipke.nl/getting-metrics-from-the-java-virtual-machine-jvm/
+	
+	private void registerAll(String prefix, MetricSet metricSet, MetricRegistry registry) {
+	    for (Entry<String, Metric> entry : metricSet.getMetrics().entrySet()) {
+	        if (entry.getValue() instanceof MetricSet) {
+	            registerAll(prefix + "." + entry.getKey(), (MetricSet) entry.getValue(), registry);
+	        } else {
+	            registry.register(prefix + "." + entry.getKey(), entry.getValue());
+	        }
+	    }
+	}
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		final Slf4jReporter reporter = Slf4jReporter.forRegistry(metricRegistry)
+		registerAll("gc", new GarbageCollectorMetricSet(), metricRegistry);
+		registerAll("buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()), metricRegistry);
+		registerAll("memory", new MemoryUsageGaugeSet(), metricRegistry);
+		registerAll("threads", new ThreadStatesGaugeSet(), threadRegistry);
+		
+		final Slf4jReporter lreporter = Slf4jReporter.forRegistry(metricRegistry)
 				.outputTo(LoggerFactory.getLogger("demo"))
 				.convertRatesTo(TimeUnit.SECONDS)
 				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
-		reporter.start(1, TimeUnit.MINUTES);
+		lreporter.start(1, TimeUnit.MINUTES);
+		
+		final Slf4jReporter tlreporter = Slf4jReporter.forRegistry(threadRegistry)
+				.outputTo(LoggerFactory.getLogger("demo"))
+				.convertRatesTo(TimeUnit.SECONDS)
+				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
+		tlreporter.start(1, TimeUnit.MINUTES);
 
 		final ConsoleReporter creporter = ConsoleReporter
 				.forRegistry(metricRegistry).convertRatesTo(TimeUnit.SECONDS)
 				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
 		creporter.start(1, TimeUnit.MINUTES);
+		
+		final ConsoleReporter tcreporter = ConsoleReporter
+				.forRegistry(threadRegistry).convertRatesTo(TimeUnit.SECONDS)
+				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
+		tcreporter.start(1, TimeUnit.MINUTES);
 
 	}
 
