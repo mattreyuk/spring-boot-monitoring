1eddd57d421f2a152538c0f72e54e8bd54d81b4e
Matt Reynolds <matthew.reynolds@staples.com>
2015-03-09 12:00:15 -0600

    add slf4j metrics logging example

--- src/main/java/demo/AppConfig.java
+++ src/main/java/demo/AppConfig.java
@@ -2,12 +2,14 @@ package demo;
 
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
 
 @Component
 public class AppConfig implements InitializingBean {
@@ -17,10 +19,16 @@ public class AppConfig implements InitializingBean {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		final ConsoleReporter reporter = ConsoleReporter
-				.forRegistry(metricRegistry).convertRatesTo(TimeUnit.SECONDS)
+		final Slf4jReporter reporter = Slf4jReporter.forRegistry(metricRegistry)
+				.outputTo(LoggerFactory.getLogger("demo"))
+				.convertRatesTo(TimeUnit.SECONDS)
 				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
 		reporter.start(1, TimeUnit.MINUTES);
+
+		final ConsoleReporter creporter = ConsoleReporter
+				.forRegistry(metricRegistry).convertRatesTo(TimeUnit.SECONDS)
+				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
+		creporter.start(1, TimeUnit.MINUTES);
 
 	}
 
--- src/main/java/demo/MonitoringClientApplication.java
+++ src/main/java/demo/MonitoringClientApplication.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MonitoringClientApplication {
-    public static final String CLIENT_NAME = "monitorClientApp";
+	  private static final Logger LOGGER = LoggerFactory.getLogger(MonitoringClientApplication.class);
     
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        LOGGER.info("responding with: "+name);
     	return "Hello "+name;
     }
 
